6c17f576cb51ece332a1eaac8510c48f81528060
OtorSama <jemwelautor@gmail.com>
2018-03-31 15:35:40 +0800

    first ever commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dog Bread Classifier</title>
+	</head>
+	<body>
+		<p>This is a sample body</p>
+		<?php 
+			$var1 = exec("python testpython.py hello.jpeg");
+			echo $var1;
+
+			$var2 = exec("python testpython.py world.jpeg");
+			echo $var2;
+		?>
+	</body>
+</html>
