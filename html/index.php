2fff7a89860b7b860cadcf95c240e87c8015f0aa
jeffrxn <obero.jeffersonbutch.ssc10@gmail.com>
2018-04-17 00:10:12 +0800

    fixing bootstrap and jquery structure

--- html/index.php
+++ html/index.php
@@ -6,8 +6,15 @@
 <html>
 	<head>
 		<title>Dog Bread Classifier</title>
+		<?php require("require.php"); ?>
+		<script>
+			$(document).ready(function() {
+		            $(".dropdown-toggle").dropdown();
+		        });
+		</script>
 	</head>
 	<body>
+		<?php require("navbar.php"); ?>
 		<p>This is a sample body</p>
 		<?php 
 			$var1 = exec("python testpython.py hello.jpeg");
---
+++ html/require.php
@@ -0,0 +1,8 @@
+<link rel="stylesheet" type="text/css" href="../bootstrap/css/bootstrap.css" />
+<script href="../bootstrap/js/jquery.js"></script>
+<script href="../bootstrap/js/bootstrap.js"></script>
+<script>
+    $(document).ready(function () {
+        $('.dropdown-toggle').dropdown();
+    });
+</script>
