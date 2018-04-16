a796206aaecda294d3bd0444a0e1960017c46751
OtorSama <jemwelautor@gmail.com>
2018-04-17 00:24:12 +0800

    '.'

--- index.php
+++ index.php
@@ -15,6 +15,11 @@
 			<button id="predictButton" type="button">Predict</button>
 		</div>
 		<div id="output"></div>
+		<p id="p1"></p>
+		<p id="p2"></p>
+		<p id="p3"></p>
+		<p id="p4"></p>
+		<p id="p5"></p>
 
 	</body>
 	<!-- Scripts -->
@@ -29,9 +34,16 @@
 			$.ajax({
 				url: "html/prediction.php?image_file="+image_file,
 				type: "get",
-				dataType: "html",
+				dataType: "JSON",
 				success: function( returnData ){
-					$("#output").html(returnData);
+					//$("#output").html(returnData);
+					console.log(returnData);
+					$("#p1").html(returnData[0]["breed_name"]);
+					$("#p2").html(returnData[1]["breed_name"]);
+					$("#p3").html(returnData[2]["breed_name"]);
+					$("#p4").html(returnData[3]["breed_name"]);
+					$("#p5").html(returnData[4]["breed_name"]);
+
 				}
 			});
 		}
