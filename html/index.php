29bc4cdf6b8b675dcd34a084c28e884de490f4ed
jeffrxn <obero.jeffersonbutch.ssc10@gmail.com>
2018-04-23 19:35:47 +0800

    Integrating Croppie, part 1

--- html/index.php
+++ html/index.php
@@ -22,12 +22,65 @@
 			<div class="row">
 				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 				<div id="imagePanel" class="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+					<!--
 					<div>
 						<form action="upload.php" method="post" enctype="multipart/form-data">
 							<input name="imageFile" type="file" id="imageFile">
 							<input type="submit" value="Upload Image" name="submit">
 						</form>
 						<button id="predictButton" type="button">Predict</button>
+					</div>
+					-->
+					<div id="croppie-sample" style="border: 0.1px solid red; padding: 10%">
+						<style>
+							#upload-actions  input[type="file"]{
+								position: absolute;
+								display: none;
+								border: 1px solid blue;
+							}
+						</style>
+
+						<script>
+							function browseImg(){
+								var inputElement = document.getElementById("imgFile");
+								inputElement.click();
+
+								inputElement.onchange = function(){
+									var file = inputElement.files[0];
+									filepath = inputElement.value.split("\\");
+									filename = filepath[filepath.length-1];
+
+									alert(filename);
+								}							
+							}
+						</script>
+
+						<div id="upload-actions">
+							<!--<a class="btn file-btn btn-default">
+								<span>Upload Image</span>
+								<input hidden type="file" value="Choose a file" accept="image/*" />
+
+
+							</a>-->
+							<button type="button" class="btn btn-default" onclick="browseImg();">Choose Image</button>
+							<input type="file" id="imgFile" name="imgFile" accept="image/*" />
+							
+						</div>
+						
+						<script>
+							$uploadImg = $('imagePanel').croppie({
+								enableExif: true,
+								viewport: {
+									width: 200,
+									height: 200,
+									type: 'circle'
+								},
+								boundary: {
+									width: 300,
+									height: 300
+								}
+							});
+						</script>
 					</div>
 				</div>
 				
