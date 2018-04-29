ba74bd764117f156af73688c9473c002405297fa
OtorSama <jemwelautor@gmail.com>
2018-04-29 20:28:50 +0800

    minor changes

--- html/require.php
+++ html/require.php
@@ -5,7 +5,6 @@
 <script type="text/javascript" src="../bootstrap/js/bootstrap.js"></script>
 <script type="text/javascript" src="../Croppie-master/croppie.min.js"></script>
 <script type="text/javascript" src="../script/script.js"></script>
-<script type="text/javascript" src="../script/script2.js"></script>
 <script>
     $(document).ready(function () {
         $('.dropdown-toggle').dropdown();
