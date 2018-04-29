776e5faca7076c4e9035868be86adeb65fa1756f
OtorSama <jemwelautor@gmail.com>
2018-04-29 18:27:37 +0800

    separated script form index.php

--- html/index.php
+++ html/index.php
@@ -16,8 +16,8 @@
 				cursor: help;
 			}
 			img {
-				height: 15%;
-				width: 15%;
+				height: 10%;
+				width: 10%;
 			}
 
 		</style>
@@ -61,15 +61,13 @@
 					</div>
 				</div>
 
-				<button id="predictButton" type="button">Predict</button>
-
 				<div id="predPanel" class="col-xs-6 col-sm-6 col-md-6 col-lg-6">
 					<!-- <p><center>For predictions</center></p> -->
+					<div id="p0" class="predicted"></div>
 					<div id="p1" class="predicted"></div>
 					<div id="p2" class="predicted"></div>
 					<div id="p3" class="predicted"></div>
 					<div id="p4" class="predicted"></div>
-					<div id="p5" class="predicted"></div>
 
 					<div>
 						<button id="predictButton" type="button" style="">Predict</button>
@@ -87,68 +85,5 @@
 				<div class="col-xs-1 col-sm-1 col-md-1 col-lg-1"></div>
 			</div>
 		</div>
-
 	</body>
-	<!-- Scripts -->
-	<script>
-		$(document).ready(function() {
-
-			$(document).on("click","#predictButton", doPrediction);
-			$(document).on("click",".predicted",viewDescription);
-		});
-
-		function doPrediction(){
-			console.log("test");
-			var image_file = "dogImage.jpg";
-			$.ajax({
-				url: "prediction.php?image_file="+image_file,
-				type: "get",
-				dataType: "JSON",
-				success: function( returnData ){
-					/*
-					$("#p1").html("<img src='../images/dog_breeds/" + returnData[0]["breed_image"] + "'>" + returnData[0]["breed_name"]);
-					$("#p2").html(returnData[1]["breed_name"]);
-					$("#p3").html(returnData[2]["breed_name"]);
-					$("#p4").html(returnData[3]["breed_name"]);
-					$("#p5").html(returnData[4]["breed_name"]);
-					*/
-					var imagePath = "../images/dog_breeds/";
-
-					for(var i = 0; i < 5; i++) {
-						var image = imagePath + returnData[i]["breed_image"];
-						var name = returnData[i]["breed_name"];
-						console.log(image);
-						$("#p"+i+"").html("<image src='"+image+ "'>" + name);
-					}
-
-					$("#p1").attr("data-id", ""+returnData[0]["breed_id"]);
-					$("#p2").attr("data-id", ""+returnData[1]["breed_id"]);
-					$("#p3").attr("data-id", ""+returnData[2]["breed_id"]);
-					$("#p4").attr("data-id", ""+returnData[3]["breed_id"]);
-					$("#p5").attr("data-id", ""+returnData[4]["breed_id"]);
-
-					$("div[name=description]").html(returnData[0]["breed_description"]);
-					$("div[name=breedname]").html(returnData[0]["breed_name"]);
-				},
-				error: function(){
-					alert("Failed");
-				}
-			});
-		}
-		function viewDescription(){
-			var id = $(this).attr("data-id");
-			$.ajax({
-				url: "get_breed_data.php?id="+id,
-				type: "get",
-				dataType: "JSON",
-				success: function(data) {
-
-					$("div[name=description]").html(data["breed_description"]);
-					$("div[name=breedname]").html(data["breed_name"]);
-				}
-			});
-		}
-
-	</script>
-	 
 </html>
---
+++ script/script2.js
@@ -0,0 +1,46 @@
+
+$(document).ready(function() {
+
+	$(document).on("click","#predictButton", doPrediction);
+	$(document).on("click",".predicted",viewDescription);
+});
+
+function doPrediction(){
+	console.log("test");
+	var image_file = "dogImage.jpg";
+	$.ajax({
+		url: "prediction.php?image_file="+image_file,
+		type: "get",
+		dataType: "JSON",
+		success: function( returnData ){
+			var imagePath = "../images/dog_breeds/";
+
+			for(var i = 0; i < 5; i++) {
+				var image = imagePath + returnData[i]["breed_image"];
+				var name = returnData[i]["breed_name"];
+
+				$("#p"+i+"").html("<image src='"+image+ "'>" + name);
+				$("#p"+i+"").attr("data-id", ""+returnData[i]["breed_id"]);
+			}
+
+			$("div[name=description]").html(returnData[0]["breed_description"]);
+			$("div[name=breedname]").html(returnData[0]["breed_name"]);
+		},
+		error: function(){
+			alert("Please Try Again.");
+		}
+	});
+}
+function viewDescription(){
+	var id = $(this).attr("data-id");
+	$.ajax({
+		url: "get_breed_data.php?id="+id,
+		type: "get",
+		dataType: "JSON",
+		success: function(data) {
+
+			$("div[name=description]").html(data["breed_description"]);
+			$("div[name=breedname]").html(data["breed_name"]);
+		}
+	});
+}
