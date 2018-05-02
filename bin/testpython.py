1df08ec2005293ef2bdbf33fc1c50a4b3709116b
OtorSama <jemwelautor@gmail.com>
2018-05-02 13:55:01 +0800

    char

--- bin/testpython.py
+++ bin/testpython.py
@@ -1,8 +1,5 @@
 import sys
 
-def main():
-	#print sys.argv[0]
-	print sys.argv[1]
-
-if __name__ == "__main__":
-	main()
+import os 
+output = os.system('python ../../../../../home/user/Downloads/code/sp/deletelater.py')
+print(output)
--- html/prediction.php
+++ html/prediction.php
@@ -1,21 +1,27 @@
 <?php
 	require("config.php");
 
-/*	$image_file = $_GET['image_file'];
-	$content = exec("python ../bin/predictBreed.py ".$image_file); */
-	$content = exec("cd ../../../../../home/user/Downloads/code/sp/; python deployPredict.py chihuahua.jpg"); 
-       	echo $content;
+	//$image_file = $_GET['image_file'];
+	$image_file = "Chihuahua.jpg";
+	//$content = exec("python ../bin/predictBreed.py ".$image_file); 
+	$content = exec("python ../bin/testpython.py"); 
+	
+	//$image_path = "/opt/lampp/htdocs/SP/images/temp".$image_file;
+	//$deploy_path = "/home/user/Downloads/code/sp/deployPredict.py";
+	//$content = exec("python ../../../../../home/user/Downloads/code/sp/deployPredict.py /opt/lampp/htdocs/SP/images/temp/Chihuahua.jpg");
+	//$content = exec("python ../../../../../home/user/Downloads/code/sp/deletelater.py");  
+       	echo "this ".$content."--";
 	$predictions = explode(" ",$content);
 	
 	$data = [];
 
-	foreach($predictions as $pred){
+	/*foreach($predictions as $pred){
 		$query = "SELECT * FROM dog_breeds
 				WHERE breed_id = {$pred}";
 
 		$result = $SP_db->query($query);
 		$data[] = $result->fetch_assoc();
-	}
+	} */
 /*
 	echo "<table border = '1'>";
 	foreach($data as $d){
