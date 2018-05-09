9d6f01767f684401169420dd1bef1a09a2c12df1
OtorSama <jemwelautor@gmail.com>
2018-05-09 20:42:50 +0800

    dogbreedsPage.php

---
+++ html/dogBreedsPage.php
@@ -0,0 +1,15 @@
+<?
+	require("config.php");
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Dog Bread Classifier</title>
+		<?php require('require.php'); ?>
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	</head>
+	<body>
+		<?php require('navbar.php'); ?>
+	</body>
+</html>
